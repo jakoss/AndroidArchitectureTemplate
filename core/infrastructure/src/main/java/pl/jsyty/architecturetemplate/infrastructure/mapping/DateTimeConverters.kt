6db9c4da87570fa91c58e56ba08d211fb4bc42a5
Jakub Syty <jakub.nekro@gmail.com>
2023-05-18 09:23:05 +0200

    Migrate kmapper to Konvert (#105)
    * Initial kmapper -> konvert migration (not working yet)
    
    * Upgrade konvert to 1.5.0
    
    * Upgrade Konvert to 1.5.1
    
    * Upgrade orbitmvi to 5
    
    * Use LF EOL

--- core/infrastructure/src/main/java/pl/jsyty/architecturetemplate/infrastructure/mapping/DateTimeConverters.kt
+++ core/infrastructure/src/main/java/pl/jsyty/architecturetemplate/infrastructure/mapping/DateTimeConverters.kt
@@ -1,17 +1,15 @@
 package pl.jsyty.architecturetemplate.infrastructure.mapping
 
+import io.mcarle.konvert.api.Konverter
 import java.time.*
 
 /**
  * In our case the [OffsetDateTime] we're getting from the api represents time in UTC.
  * We can convert that to [LocalDateTime] just by moving that by the offset of current zone to UTC ([localZoneOffset] field stores that offset).
  */
+@Konverter
 interface DateTimeConverters {
-    fun asLocalDateTime(offsetDateTime: OffsetDateTime?): LocalDateTime? =
-        offsetDateTime?.toLocalDateTime()
-
-    fun asOffsetDateTime(localDateTime: LocalDateTime?): OffsetDateTime? =
-        localDateTime?.atOffset(localZoneOffset)
+    fun asOffsetDateTime(source: LocalDateTime): OffsetDateTime = source.atOffset(localZoneOffset)
 
     companion object {
         val localZoneOffset: ZoneOffset by lazy { OffsetDateTime.now().offset }
--- data/weather/impl/src/main/java/pl/jsyty/architecturetemplate/data/weather/impl/WeatherRepositoryImpl.kt
+++ data/weather/impl/src/main/java/pl/jsyty/architecturetemplate/data/weather/impl/WeatherRepositoryImpl.kt
@@ -14,7 +14,6 @@ class WeatherRepositoryImpl @Inject constructor(
     override suspend fun getCurrentWeather(latitude: Double, longitude: Double): CurrentWeather {
         val weather =
             weatherService.getWeather(latitude, longitude, currentWeather = true, timezone = "UTC")
-
         return weatherMapper.fromDto(weather.currentWeather)
     }
 }

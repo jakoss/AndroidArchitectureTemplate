dab79d66aaca1b6cfd93b960d9923ccc037f87c2
Jakub Syty <jakub.nekro@gmail.com>
2023-06-12 11:58:39 +0200

    We don't need @Konverter annotation for interfaces with default impl now

--- core/infrastructure/src/main/java/pl/jsyty/architecturetemplate/infrastructure/mapping/DateTimeConverters.kt
+++ core/infrastructure/src/main/java/pl/jsyty/architecturetemplate/infrastructure/mapping/DateTimeConverters.kt
@@ -1,13 +1,11 @@
 package pl.jsyty.architecturetemplate.infrastructure.mapping
 
-import io.mcarle.konvert.api.Konverter
 import java.time.*
 
 /**
  * In our case the [OffsetDateTime] we're getting from the api represents time in UTC.
  * We can convert that to [LocalDateTime] just by moving that by the offset of current zone to UTC ([localZoneOffset] field stores that offset).
  */
-@Konverter
 interface DateTimeConverters {
     fun asOffsetDateTime(source: LocalDateTime): OffsetDateTime = source.atOffset(localZoneOffset)
 
